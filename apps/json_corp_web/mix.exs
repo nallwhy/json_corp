d37d411cbb175a380255d6af12589f6bc9997974
Jinkyou Son <nallwhy@gmail.com>
2022-06-07 23:05:45 +0900

    Add timeout to deps

--- apps/json_corp_web/mix.exs
+++ apps/json_corp_web/mix.exs
@@ -50,7 +50,8 @@ defmodule JsonCorpWeb.MixProject do
       {:gettext, "~> 0.18"},
       {:json_corp, in_umbrella: true},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:timeout, github: "nallwhy/timeout"}
     ]
   end
 
