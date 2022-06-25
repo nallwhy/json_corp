b07c66765a87b6d15a6c69ad858ab725a18e908a
Jinkyou Son <nallwhy@gmail.com>
2022-06-25 19:59:52 +0900

    Show posts on blog post index page

--- apps/json_corp_web/lib/json_corp_web/live/blog/post_live.index.ex
+++ apps/json_corp_web/lib/json_corp_web/live/blog/post_live.index.ex
@@ -1,13 +1,25 @@
 defmodule JsonCorpWeb.Blog.PostLive.Index do
   use JsonCorpWeb, :live_view
+  alias JsonCorp.Blog
+  alias JsonCorp.Blog.Post
 
   def mount(_params, _session, socket) do
+    socket = socket |> assign_new(:posts, fn -> Blog.list_posts() end)
+
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    Blog List
+    Post List
+
+    <div>
+    <%= for %Post{title: title} <- @posts do %>
+      <div>
+        <p>title: <%= title %></p>
+      </div>
+    <% end %>
+    </div>
     """
   end
 end
