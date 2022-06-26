04dc6cab6f37c5a7fbe93af7e8ca24cc644cf1fa
Jinkyou Son <nallwhy@gmail.com>
2022-06-26 18:48:20 +0900

    Implement list_posts/2 of JsonCorp.Blog

--- apps/json_corp/lib/json_corp/blog/blog.ex
+++ apps/json_corp/lib/json_corp/blog/blog.ex
@@ -1,10 +1,35 @@
 defmodule JsonCorp.Blog do
   alias JsonCorp.Blog.Post
 
+  @posts_path Application.app_dir(:json_corp, "priv/posts")
+
   @spec list_posts :: [%Post{title: String.to(), body: String.t() | nil}]
   def list_posts() do
-    [
-      %Post{title: "test", body: nil}
-    ]
+    list_post_paths()
+    |> Enum.map(&read_post/1)
+  end
+
+  defp list_post_paths() do
+    @posts_path
+    |> File.ls!()
+    |> Enum.filter(fn filename -> String.ends_with?(filename, ".md") end)
+    |> Enum.sort()
+    |> Enum.map(fn filename -> @posts_path <> "/" <> filename end)
+  end
+
+  defp read_post(path) do
+    path
+    |> File.read!()
+    |> parse_post()
+  end
+
+  defp parse_post(raw_post) do
+    [meta_str, body] =
+      raw_post
+      |> String.split("---", part: 2, trim: true)
+
+    {meta, _binding} = meta_str |> Code.eval_string()
+
+    %Post{title: meta |> Map.get(:title), body: body}
   end
 end
