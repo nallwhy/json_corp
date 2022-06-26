193a5c22cedcb22f065297a4a1d6ae40ba5a9f84
Jinkyou Son <nallwhy@gmail.com>
2022-06-26 21:49:57 +0900

    Render markdown on blog post page

--- apps/json_corp_web/lib/json_corp_web/live/blog/post_live.show.ex
+++ apps/json_corp_web/lib/json_corp_web/live/blog/post_live.show.ex
@@ -2,6 +2,7 @@ defmodule JsonCorpWeb.Blog.PostLive.Show do
   use JsonCorpWeb, :live_view
   alias JsonCorp.Blog
   alias JsonCorp.Blog.Post
+  alias JsonCorp.Blog.MarkdownRenderer
 
   def mount(%{"slug" => slug}, _session, socket) do
     socket = socket |> assign_new(:post, fn -> load_post(slug) end)
@@ -13,7 +14,9 @@ defmodule JsonCorpWeb.Blog.PostLive.Show do
     ~H"""
     <div>
       <h1><%= @post.title %></h1>
-      <p><%= @post.body %></p>
+      <div>
+        <%= @post.body |> MarkdownRenderer.html() |> raw() %>
+      </div>
     </div>
     """
   end
