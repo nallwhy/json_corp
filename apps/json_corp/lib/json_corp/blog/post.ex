b07731531c7247d4b4f1ae24221b5603702d7161
Jinkyou Son <nallwhy@gmail.com>
2022-08-05 01:06:06 +0900

    Add type to JsonCorp.Blog.Post

--- apps/json_corp/lib/json_corp/blog/post.ex
+++ apps/json_corp/lib/json_corp/blog/post.ex
@@ -1,4 +1,10 @@
 defmodule JsonCorp.Blog.Post do
+  @type t :: %__MODULE__{
+          title: String.t(),
+          slug: String.t(),
+          body: String.t()
+        }
+
   @enforce_keys [:slug, :title, :body]
   defstruct @enforce_keys
 end
