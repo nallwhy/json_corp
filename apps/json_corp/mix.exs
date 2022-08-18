4e8816a13c0edc2089d59620557867f624de7389
Jinkyou Son <nallwhy@gmail.com>
2022-08-18 20:46:09 +0900

    Add timex to deps

--- apps/json_corp/mix.exs
+++ apps/json_corp/mix.exs
@@ -43,7 +43,8 @@ defmodule JsonCorp.MixProject do
       {:swoosh, "~> 1.3"},
       {:earmark, "~> 1.4"},
       {:makeup, "~> 1.1"},
-      {:makeup_elixir, "~> 0.16.0"}
+      {:makeup_elixir, "~> 0.16.0"},
+      {:timex, "~> 3.7"}
     ]
   end
 
