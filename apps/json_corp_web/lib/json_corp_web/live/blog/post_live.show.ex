4ea641cac7763ce566c87c98278a1b9b7552f724
Jinkyou Son <nallwhy@gmail.com>
2022-08-20 11:01:45 +0900

    Change link/2 to live_redirect/2

--- apps/json_corp_web/lib/json_corp_web/live/blog/post_live.show.ex
+++ apps/json_corp_web/lib/json_corp_web/live/blog/post_live.show.ex
@@ -19,7 +19,7 @@ defmodule JsonCorpWeb.Blog.PostLive.Show do
     ~H"""
     <div>
       <div>
-        <%= link "<- 전체 글 보기", to: Routes.blog_post_index_path(@socket, :index) %>
+        <%= live_redirect "<- 전체 글 보기", to: Routes.blog_post_index_path(@socket, :index) %>
       </div>
       <div class="prose">
         <h1><%= @post.title %></h1>
