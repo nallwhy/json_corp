15d306a8213857ce6b053dd0113bb3f1bf020e42
Jinkyou Son <nallwhy@gmail.com>
2022-09-23 21:17:23 +0900

    Setup release

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule JsonCorp.Umbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      releases: releases()
     ]
   end
 
@@ -40,6 +41,17 @@ defmodule JsonCorp.Umbrella.MixProject do
     [
       # run `mix setup` in all child apps
       setup: ["cmd mix setup"]
+    ]
+  end
+
+  defp releases do
+    [
+      json_corp_app: [
+        applications: [
+          json_corp: :permanent,
+          json_corp_web: :permanent
+        ]
+      ]
     ]
   end
 end
