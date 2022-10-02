700a368535d66deba42991698f8f45c32cafdc76
Jinkyou Son <nallwhy@gmail.com>
2022-10-02 23:41:39 +0900

    Show view count in blog post show page

--- apps/json_corp_web/lib/json_corp_web/live/blog/post_live.show.ex
+++ apps/json_corp_web/lib/json_corp_web/live/blog/post_live.show.ex
@@ -3,12 +3,14 @@ defmodule JsonCorpWeb.Blog.PostLive.Show do
   alias JsonCorp.Blog
   alias JsonCorp.Blog.Post
   alias JsonCorp.Blog.MarkdownRenderer
+  alias JsonCorp.Stats
 
   @impl true
   def mount(%{"slug" => slug}, _session, socket) do
     socket =
       socket
       |> assign(:post, load_post(slug))
+      |> assign(:view_count, nil)
       |> assign_page_meta(fn %{
                                post: %{
                                  title: title,
@@ -23,6 +25,18 @@ defmodule JsonCorpWeb.Blog.PostLive.Show do
   end
 
   @impl true
+  def handle_params(_params, uri, socket) do
+    normalized_uri =
+      uri |> URI.parse() |> Map.merge(%{fragment: nil, query: nil}) |> URI.to_string()
+
+    socket =
+      socket
+      |> assign(:view_count, load_view_count(normalized_uri))
+
+    {:noreply, socket}
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
     <div class="px-8 py-4">
@@ -34,6 +48,7 @@ defmodule JsonCorpWeb.Blog.PostLive.Show do
           <h1><%= @post.title %></h1>
           <p><%= @post.description %></p>
           <div><time>Date created: <%= @post.date_created %></time></div>
+          <div>View count: <%= @view_count %></div>
         </div>
       </div>
       <hr class="prose">
@@ -51,5 +66,9 @@ defmodule JsonCorpWeb.Blog.PostLive.Show do
     {:ok, %Post{} = post} = Blog.fetch_post(slug)
 
     post
+  end
+
+  def load_view_count(uri) do
+    Stats.get_view_count_by_uri(uri)
   end
 end
