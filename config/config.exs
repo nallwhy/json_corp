4b23e05d189f8d66a1d1df27087d8375ada391a8
Jinkyou Son <nallwhy@gmail.com>
2022-10-06 10:57:13 +0900

    Setup stats to Cache.Local

--- config/config.exs
+++ config/config.exs
@@ -65,7 +65,7 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-config :json_corp, JsonCorp.Core.Cache.Local, []
+config :json_corp, JsonCorp.Core.Cache.Local, stats: true
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
