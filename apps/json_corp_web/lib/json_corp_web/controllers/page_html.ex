59494e1d47f73c3afabe99e8ceb3a2417195be65
Jinkyou Son <nallwhy@gmail.com>
2023-01-15 10:59:39 +0900

    Create project_card component

--- apps/json_corp_web/lib/json_corp_web/controllers/page_html.ex
+++ apps/json_corp_web/lib/json_corp_web/controllers/page_html.ex
@@ -2,4 +2,9 @@ defmodule JsonCorpWeb.PageHTML do
   use JsonCorpWeb, :html
 
   embed_templates "page_html/*"
+
+  attr :name, :string, required: true
+  attr :description, :string, required: true
+  attr :url, :string, required: true
+  def project_card(assigns)
 end
