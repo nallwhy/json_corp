d951606a8193f8723c6627420144314507ee0c60
Jinkyou Son <nallwhy@gmail.com>
2023-01-21 14:12:55 +0900

    Fix tests

--- apps/json_corp_web/test/json_corp_web/views/error_view_test.exs
+++ apps/json_corp_web/test/json_corp_web/views/error_view_test.exs
@@ -5,10 +5,10 @@ defmodule JsonCorpWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(JsonCorpWeb.ErrorView, "404.html", []) == "Not Found"
+    assert render_to_string(JsonCorpWeb.ErrorHTML, "404.html", []) == "Not Found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(JsonCorpWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(JsonCorpWeb.ErrorHTML, "500.html", []) == "Internal Server Error"
   end
 end
