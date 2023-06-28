dda45102e03226e1feafca50f2ffe03c4874c20e
Jinkyou Son <nallwhy@gmail.com>
2023-06-28 16:33:08 +0900

    Setup Cldr.Plug.PutLocale, Cldr.Plug.PutSession plugs

--- config/config.exs
+++ config/config.exs
@@ -75,6 +75,9 @@ config :json_corp, JsonCorp.Core.Cache.Local, stats: true
 
 config :json_corp, :slack, webhook_url: ""
 
+config :ex_cldr,
+  default_backend: JsonCorp.Core.Cldr
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
