80bd366bba5da1037a531b8c5929771b839157be
Jinkyou Son <nallwhy@gmail.com>
2023-08-24 05:39:18 +0900

    Add sitemap to static paths

--- apps/json_corp_web/lib/json_corp_web.ex
+++ apps/json_corp_web/lib/json_corp_web.ex
@@ -17,7 +17,7 @@ defmodule JsonCorpWeb do
   and import those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon apple-touch-icon robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon apple-touch-icon robots.txt sitemap)
 
   def router do
     quote do
