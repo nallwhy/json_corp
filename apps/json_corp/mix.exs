e70d7be6cfeb49e2726dbeccae23d96ba364a124
Jinkyou Son <nallwhy@gmail.com>
2023-09-23 06:39:34 +0900

    Add --force-drop option to ecto.reset mix command alias

--- apps/json_corp/mix.exs
+++ apps/json_corp/mix.exs
@@ -64,7 +64,7 @@ defmodule JsonCorp.MixProject do
     [
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      "ecto.reset": ["ecto.drop --force-drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "release.setup": []
     ]
