29b8cccad09bbcb613d04ad181a7c052761c277f
Jinkyou Son <nallwhy@gmail.com>
2023-09-28 21:45:04 +0900

    Implement showing others cursor

--- apps/json_corp_web/lib/json_corp_web/channels/presence.ex
+++ apps/json_corp_web/lib/json_corp_web/channels/presence.ex
@@ -8,4 +8,37 @@ defmodule JsonCorpWeb.Presence do
   use Phoenix.Presence,
     otp_app: :json_corp_web,
     pubsub_server: JsonCorp.PubSub
+
+  def sync_presence_diff(presences, %{joins: joins, leaves: leaves}) do
+    presences =
+      joins
+      |> Enum.reduce(presences, fn {key, %{metas: metas}}, presences ->
+        metas
+        |> Enum.reduce(presences, fn %{phx_ref: phx_ref} = meta, presences ->
+          presences
+          |> Map.update(key, %{metas: [meta]}, fn %{metas: old_metas} ->
+            case Enum.find_index(old_metas, &(&1.phx_ref == phx_ref)) do
+              nil -> %{metas: [meta | old_metas]}
+              index -> %{metas: old_metas |> List.replace_at(index, meta)}
+            end
+          end)
+        end)
+      end)
+
+    _presences =
+      leaves
+      |> Enum.reduce(presences, fn {key, %{metas: metas}}, presences ->
+        metas
+        |> Enum.reduce(presences, fn %{phx_ref: phx_ref}, presences ->
+          presences
+          |> Map.update(key, %{metas: []}, fn %{metas: old_metas} ->
+            case Enum.find_index(old_metas, &(&1.phx_ref == phx_ref)) do
+              nil -> %{metas: old_metas}
+              index -> %{metas: old_metas |> List.delete_at(index)}
+            end
+          end)
+        end)
+      end)
+      |> Map.reject(fn {_key, %{metas: metas}} -> Enum.empty?(metas) end)
+  end
 end
--- apps/json_corp_web/lib/json_corp_web/live/cursor_live.ex
+++ apps/json_corp_web/lib/json_corp_web/live/cursor_live.ex
@@ -1,13 +1,35 @@
 defmodule JsonCorpWeb.CursorLive do
   use JsonCorpWeb, :live_view
+  alias JsonCorpWeb.Presence
+  alias JsonCorp.PubSub
 
   on_mount(JsonCorpWeb.SessionHook)
 
+  @presence_topic "cursor"
+
   @impl true
   def mount(_params, _session, socket) do
+    position = %{x: nil, y: nil, base_x: nil, base_y: nil}
+    user = %{id: socket.assigns.session_id, name: socket.assigns.session_id}
+
+    if connected?(socket) do
+      {:ok, _} =
+        Presence.track(self(), @presence_topic, user.id, %{
+          name: user.name,
+          position: position
+        })
+
+      Phoenix.PubSub.subscribe(PubSub, @presence_topic)
+    end
+
+    presences = Presence.list(@presence_topic)
+    users = presences |> convert_presences_to_users(user.id)
+
     socket =
       socket
-      |> assign(x: nil, y: nil, base_x: nil, base_y: nil)
+      |> assign(:presences, presences)
+      |> assign(:user, user)
+      |> assign(:users, users)
 
     {:ok, socket, layout: false}
   end
@@ -16,14 +38,19 @@ defmodule JsonCorpWeb.CursorLive do
   def render(assigns) do
     ~H"""
     <div id="cursor-container" phx-hook="Cursor">
-    asf
-      <ul :if={@x} class="list-none">
+      <ul :for={{user_id, user} <- @users} class="list-none">
         <li
-          style={"color: deeppink; left: #{@x + @base_x}px; top: #{@y + @base_y}px"}
+          :if={user.position.x != nil}
+          style={"color: deeppink; left: #{user.position.x + user.position.base_x}px; top: #{user.position.y + user.position.base_y}px"}
           class="flex flex-col absolute pointer-events-none whitespace-nowrap overflow-hidden"
         >
           <Icon.cursor />
-          <span style={"background-color: deeppink;"} class="mt-0 ml-2 px-2 py-1 text-sm rounded-md text-white"><%= @session_id %></span>
+          <span
+            style="background-color: deeppink;"
+            class="mt-0 ml-2 px-2 py-1 text-sm rounded-md text-white"
+          >
+            <%= user_id %>
+          </span>
         </li>
       </ul>
     </div>
@@ -31,11 +58,49 @@ defmodule JsonCorpWeb.CursorLive do
   end
 
   @impl true
-  def handle_event("cursor-move", %{"x" => x, "y" => y, "base_x" => base_x, "base_y" => base_y}, socket) do
-    socket =
-      socket
-      |> assign(x: x, y: y, base_x: base_x, base_y: base_y)
+  def handle_event(
+        "cursor-move",
+        %{"x" => x, "y" => y, "base_x" => base_x, "base_y" => base_y},
+        socket
+      ) do
+    Presence.update(self(), @presence_topic, socket.assigns.session_id, fn meta ->
+      meta |> Map.put(:position, %{x: x, y: y, base_x: base_x, base_y: base_y})
+    end)
 
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(
+        %Phoenix.Socket.Broadcast{
+          topic: @presence_topic,
+          event: "presence_diff",
+          payload: payload
+        },
+        socket
+      ) do
+    presences = Presence.sync_presence_diff(socket.assigns.presences, payload)
+    users = presences |> convert_presences_to_users(socket.assigns.user.id)
+
+    socket =
+      socket
+      |> assign(:presences, presences)
+      |> assign(:users, users)
+
+    {:noreply, socket}
+  end
+
+  defp convert_presences_to_users(presences, my_user_id) do
+    presences
+    |> Map.new(fn {user_id, presence} -> {user_id, convert_presence_to_user(presence)} end)
+    |> Map.delete(my_user_id)
+  end
+
+  defp convert_presence_to_user(nil) do
+    nil
+  end
+
+  defp convert_presence_to_user(%{metas: [last_meta | _]}) do
+    %{position: last_meta.position}
   end
 end
