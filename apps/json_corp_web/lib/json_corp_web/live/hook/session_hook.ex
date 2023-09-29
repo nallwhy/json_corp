0c0e48a92aeebb39961365c77defe60dd05acfcf
Jinkyou Son <nallwhy@gmail.com>
2023-09-29 21:44:53 +0900

    Implement assign ws_conn_id to socket per each websocket connection

--- apps/json_corp_web/lib/json_corp_web/live/hook/session_hook.ex
+++ apps/json_corp_web/lib/json_corp_web/live/hook/session_hook.ex
@@ -1,10 +1,18 @@
 defmodule JsonCorpWeb.SessionHook do
   import Phoenix.Component, only: [assign: 3]
+  import Phoenix.LiveView, only: [connected?: 1]
 
   def on_mount(:default, _params, %{"session_id" => session_id}, socket) do
+    ws_conn_id =
+      case connected?(socket) do
+        true -> socket.private.connect_params["ws_conn_id"]
+        false -> nil
+      end
+
     socket =
       socket
       |> assign(:session_id, session_id)
+      |> assign(:ws_conn_id, ws_conn_id)
 
     {:cont, socket}
   end
