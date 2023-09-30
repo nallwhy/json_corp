400922667fb0a2b4d567d7d0db74333e0240d76d
Jinkyou Son <nallwhy@gmail.com>
2023-09-30 15:46:09 +0900

    Upgrade topbar to v2.0.0 & Apply delayMillis option to topbar

--- apps/json_corp_web/assets/js/app.js
+++ apps/json_corp_web/assets/js/app.js
@@ -31,7 +31,7 @@ let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfTo
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-window.addEventListener("phx:page-loading-start", info => topbar.show())
+window.addEventListener("phx:page-loading-start", info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
 // connect if there are any LiveViews on the page
