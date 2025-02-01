ec85d4ac4820fb66aba58068c839c26e5d103b3e
Jinkyou Son <nallwhy@gmail.com>
2025-02-01 16:47:53 +0900

    Upgrade deps

--- apps/json_corp/mix.exs
+++ apps/json_corp/mix.exs
@@ -36,7 +36,7 @@ defmodule JsonCorp.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.0-rc.0", override: true},
+      {:phoenix, "~> 1.7.0"},
       {:phoenix_pubsub, "~> 2.0"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
