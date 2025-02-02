2367b4afff61c9d8567bd4870eefd57277a3b5da
Jinkyou Son <nallwhy@gmail.com>
2025-02-02 17:40:19 +0900

    Add []+() markdown grammar for new window link

--- apps/json_corp/lib/json_corp/domain/blog/markdown_renderer.ex
+++ apps/json_corp/lib/json_corp/domain/blog/markdown_renderer.ex
@@ -8,6 +8,7 @@ defmodule JsonCorp.Blog.MarkdownRenderer do
         footnotes: true
       ]
     )
+    |> customize()
     |> MDEx.to_html!(render: [unsafe_: true])
   end
 
@@ -17,5 +18,56 @@ defmodule JsonCorp.Blog.MarkdownRenderer do
 
   def plain_text(markdown) do
     markdown |> html() |> HTML.strip_tags()
+  end
+
+  defp customize(document) do
+    use Lens2
+
+    nodes_lens = Lens.repeatedly(Lens.key?(:nodes) |> Lens.all())
+
+    document
+    |> Deeply.update(nodes_lens, fn
+      %{nodes: nodes} = node ->
+        new_nodes =
+          nodes
+          |> Enum.flat_map(&parse_new_window_link/1)
+
+        %{node | nodes: new_nodes}
+
+      node ->
+        node
+    end)
+  end
+
+  defp parse_new_window_link(node) do
+    regex = ~r/(?:\[(?<name>.*?)\])?\+\((?<link>.*?)\)/
+
+    with %MDEx.Text{literal: literal} <- node,
+         true <- Regex.match?(regex, literal) do
+      Regex.split(regex, literal, include_captures: true)
+      |> Enum.map(fn text ->
+        case Regex.match?(regex, text) do
+          true ->
+            %{"name" => name, "link" => link} = Regex.named_captures(regex, text)
+
+            name =
+              case name do
+                "" -> link
+                _ -> name
+              end
+
+            link_html =
+              ~s|<a href=#{link} target="_blank" rel="noopener noreferrer">#{name}</a>|
+
+            %MDEx.HtmlInline{literal: link_html}
+
+          false ->
+            %MDEx.Text{literal: text}
+        end
+      end)
+    else
+      _ ->
+        [node]
+    end
   end
 end
