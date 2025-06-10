541fcf1279ac453d9131d1535ac72d245e88b428
Jinkyou Son <nallwhy@gmail.com>
2025-06-11 07:58:50 +0900

    Run mix test before deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -euo pipefail
+
+mix test
+
 fly deploy \
     --build-secret FLUXON_KEY_FINGERPRINT=$FLUXON_KEY_FINGERPRINT \
     --build-secret FLUXON_LICENSE_KEY=$FLUXON_LICENSE_KEY
